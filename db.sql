7373804ef15793324c8a8b5e5af58c51da740f75
mlunkan <lundqvistmarcus@gmail.com>
2013-03-07 11:26:20 +0100

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,30 @@
+drop table login;
+drop table teams;
+drop table score;
+drop table game;
+create table login(
+  id int, 
+	username varchar(20), 
+	password varchar(32)
+);
+create table teams(
+	id int, 
+	teamname varchar(100), 
+	player1name varchar(32),
+	player2name varchar(32)
+);
+create table score(
+	teamsid int, 
+	gamesplayed int, 
+	gameswon int,
+	gameslost int,
+	goalsscored int,
+	goalsagainst int,
+	points int
+);
+create table game(
+	homeid int,
+	awayid int,
+	homescore int,
+	awayscore int
+);
